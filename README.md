4020cc0dc428da5ff2b42936e8994cf2892e0348
BeauKnowsTech <beaux@lappy.localdomain>
2020-09-22 18:06:21 -0600

    v1.0 lab setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # WebApp-Lab-Setup
+
+as always, look under the hood before you run a script from the internet
+chmod +x WebApp-Lab-Setup.sh
+./WebApp-Lab-Setup.sh
---
+++ WebApp-Lab-Setup.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# This script will install vulnerable applications using Docker so that we can learn how to hack web applications
+# Version 1.0
+# Youtube: https://www.youtube.com/c/beauknowstechstuff
+# Twitter: @BeauKnowsTech
+# Only been tested in Kali
+
+# check if root cuz not supposed to be root
+if [ "$EUID" -eq 0 ]
+  then echo "This script is not meant to ran with sudo. It will ask you for your password if needed"
+  exit
+fi
+
+sudo apt update
+
+# Check and make sure git is installed
+echo -e "\nChecking to see if git is instlld already"
+if git --version ; then
+    echo -e "Git is already installed"
+    else
+    echo -e "Doesn't look like git is installed, lets fix that"
+    sudo apt install git -y
+fi
+
+## check if running on wsl and if docker is installed 
+echo -e "\nChecking to see if running on WSL \n"
+if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null ; then
+    echo -e "Running on WSL Windows 10 \n"
+    if docker > /dev/null 2>&1 ; then
+        echo -e "Docker is running \n"
+    else
+        echo "Docker is either not running or not installed."
+        echo "At the time of writing, Docker recommends that if you are running WSL to install Docker Desktop"
+        echo "Docker Desktop has a built in WSL integration. I'd go with WSL2"
+        echo "See https://docs.docker.com/docker-for-windows/wsl/ for details."
+        exit
+    fi
+else
+    echo "Not Running on WSL, lets keep going"
+    if docker > /dev/null 2>&1 ; then
+    echo -e "Docker is running \n"
+    else
+    echo "Installing docker"
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.list
+    apt remove docker docker-engine docker.io
+    apt install -y docker-ce
+    fi
+fi
+
+# install zsh + OhMyzsh
+echo -e "Installing ZSH and ohmyzsh, if not already installed \n"
+
+if zsh --version ; then
+    echo -e "Looks like ZSH is already installed. Very nice"
+    else
+    echo -e "ZSH not detected, installing ZSH and ohmyzsh"
+    sudo apt install zsh -y
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # set zsh as default shell
+    echo "Attempting to set zsh as default shell"
+    chsh -s $(which zsh)
+fi
+
+
+# Install vulnerable docker containers
+echo -e "\nInstalling vulnerable docker containers Juice Shop, Webgoat, DVWA"
+tz=$(cat /etc/timezone)
+docker run --name juice-shop --restart unless-stopped -d -p 3000:3000 bkimminich/juice-shop
+docker run --name goatandwolf --restart unless-stopped -d -p 8080:8080 -p 9090:9090 -e TZ=$tz webgoat/goatandwolf
+docker run --name web-dvwa --restart unless-stopped -d -p 80:80 vulnerables/web-dvwa
+
+
+
+#todo
+# install any tools we still need
+
